88e71fca0c5150c916c87f1696bb4b700a8459a6
abdullah alowais <m3mol@192.168.1.14>
2024-03-21 17:20:15 +0300

    updated seccussfully

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
-(function (){
+(function () {
     'use strict';
-
+  
     angular.module('LunchCheck', [])
-        .controller('LunchCheckController', LunchCheckController);
-
+      .controller('LunchCheckController', LunchCheckController);
+  
     LunchCheckController.$inject = ['$scope'];
-        
-        function LunchCheckController($scope) {
-            $scope.checkItems = function () {
-              var items = $scope.lunchItems;
-              // Implement functionality to check lunch items and update message
-              if (items) {
-                var itemArray = items.split(',');
-                if (itemArray.length <= 3) {
-                  $scope.message = "Enjoy!";
-                } else {
-                  $scope.message = "Too much!";
-                }
-              } else {
-                $scope.message = "Please enter data first";
-              }
-            };
+  
+    function LunchCheckController($scope) {
+      $scope.checkItems = function () {
+        var items = $scope.lunchItems;
+        // Implement functionality to check lunch items and update message
+        if (items) {
+          var itemArray = items.split(',');
+          var itemCount = 0;
+          for (var i = 0; i < itemArray.length; i++) {
+            // Trim each item to remove leading and trailing spaces
+            var item = itemArray[i].trim();
+            // Check if the item is not an empty string and not an empty item
+            if (item !== "" && item !== ",") {
+              itemCount++;
+            }
           }
-})();
+          if (itemCount <= 3) {
+            $scope.message = "Enjoy!";
+          } else {
+            $scope.message = "Too much!";
+          }
+        } else {
+          $scope.message = "Please enter data first";
+        }
+      };
+    }
+  })();
+  
